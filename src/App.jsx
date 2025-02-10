5881f2279ea89cb8decc689a18b0df8f56e05ce2
raghubng <raghavendranaikeh@gmail.com>
2025-02-10 12:31:01 +0530

    deploy todo app to github

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { useState } from "react";
+import logo from "./assets/logo.png";
+import Todos from './Components/Todos';
+import Todo from './Components/Todo';
+import './App.css';
+function App() {
+
+
+  return (
+ 
+    <>
+    {/* <Todos/> */}
+    <Todo/>
+  
+   </>
+      
+    
+  );
+}
+
+export default App
